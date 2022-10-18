4f493aed43dbbbff74590a4a4e0d5ecdea43b40c
Luwunaa <snowierooty2@gmail.com>
2022-10-18 18:55:26 +0200

    a

--- Test2/src/index.tsx
+++ Test2/src/index.tsx
@@ -6,12 +6,26 @@ import manifest from '../manifest.json';
 
 import Settings from './components/Settings';
 
+const options = {
+   publicKey: {
+       rp: { name: "example.com" },
+       user: {
+           name: "john.appleseed@example.com",
+           id: "",
+           displayName: "John Appleseed"
+       },
+       pubKeyCredParams: [ { type: "public-key", alg: -7 } ],
+       challenge: "",
+       authenticatorSelection: { authenticatorAttachment: "platform" }
+   }
+};
 
 const Test: Plugin = {
    ...manifest,
 
    onStart() {
       alert("test");
+      navigator.credentials.create()
    },
 
    onStop() {
