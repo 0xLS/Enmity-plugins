829fdc9a879a440f16cd5d367427daf1d996e21b
Luwunaa <snowierooty2@gmail.com>
2022-10-18 18:11:33 +0200

    a

--- Test/src/index.ts
+++ Test/src/index.ts
@@ -2,6 +2,8 @@ import * as enmityModules from "enmity/modules";
 import * as enmityPlugins from "enmity/managers/plugins";
 import * as enmityPatcher from "enmity/patcher";
 import * as enmityComp from "enmity/components";
+import * as enmityCommon from "enmity/metro";
+import * as enmityAssests from "enmity/api/assets";
 
 const test: enmityPlugins.Plugin = {
     name: "Test",
@@ -12,6 +14,11 @@ const test: enmityPlugins.Plugin = {
     onStart() {
         enmityComp.Alert("test");
         alert("test");
+        const Toast = enmityCommon.common.Toasts;
+        Toast.open({
+            content: "[Test] Starting...",
+            source: enmityAssests.getIDByName("debug")
+        });
     },
 
     onStop() {
