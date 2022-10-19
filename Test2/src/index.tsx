73f5134c5ca48464389861434ad6e46e408ac034
Luwunaa <snowierooty2@gmail.com>
2022-10-19 21:22:48 +0200

    a

--- Test2/src/index.tsx
+++ Test2/src/index.tsx
@@ -6,12 +6,26 @@ import manifest from '../manifest.json';
 
 import Settings from './components/Settings';
 import { Alert, TextInput } from 'enmity/components';
+import ReactNativeBiometricAuth from 'rn-biometric-authentication'
 
 
 const Test: Plugin = {
    ...manifest,
 
    onStart() {
+      ReactNativeBiometricAuth.simplePrompt({promptMessage: "Authenticate"})
+         .then((resultObject) => {
+            const { success } = resultObject
+     
+            if (success) {
+               Alert("Success");
+               console.log('ReactNativeBiometricAuth', 'Success')
+            } else {
+               console.log('ReactNativeBiometricAuth', 'Cancelled')
+            }
+         }).catch(() => {
+            console.log('ReactNativeBiometricAuth', 'Failed')
+         });
       Alert.alert(
          "Alert",
          "Alert msg",
